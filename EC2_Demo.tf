ea9e6eaac6be9a48e5ebd9585fe1380560255ab2
raghupuuvk <raghupuuvk20@gmail.com>
2024-07-10 21:27:50 +0530

    Add files via upload

---
+++ EC2_Demo.tf
@@ -0,0 +1,46 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.50.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+ region = "us-east-1"
+}
+
+
+resource "aws_instance" "LinuxPublic_call" {
+  ami           = "ami-0bb84b8ffd87024d8"
+  instance_type = "t2.micro"
+  key_name = "04May2024"
+
+  tags = {
+    Name = "LinuxPublic1"
+  }
+}
+
+
+resource "aws_security_group" "Security_group_call" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "Linux_Security_Group"
+  }
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "Security_group_ingress_call" {
+  security_group_id = aws_security_group.Security_group_call.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = "All_Traffic"
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
